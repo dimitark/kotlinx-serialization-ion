ab82aa513d7ba41df10c33b12adc3c050d9c7bd5
Dimitar Kotevski <dko@unwire.com>
2021-09-28 15:26:35 +0200

    Removed an unnecessary test

--- src/test/kotlin/com/github/dimitark/kotlix/serialization/ion/IntegrityCheckTest.kt
+++ src/test/kotlin/com/github/dimitark/kotlix/serialization/ion/IntegrityCheckTest.kt
@@ -3,9 +3,6 @@ package com.github.dimitark.kotlix.serialization.ion
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.modules.SerializersModule
-import kotlinx.serialization.modules.polymorphic
-import kotlinx.serialization.modules.subclass
 import kotlin.test.Test
 import kotlin.test.assertTrue
 
@@ -30,44 +27,6 @@ class IntegrityCheckTest {
 
         assertTrue(integrityFailed)
     }
-
-    @Test
-    fun `Test the integrity check with polymorphism`() {
-        val serializationIon = Ion {
-            serializersModule = SerializersModule {
-                polymorphic(Person::class) {
-                    subclass(Student::class)
-                    subclass(Employee::class)
-                }
-            }
-        }
-
-        val deserializationIon = Ion {
-            serializersModule = SerializersModule {
-                polymorphic(Person::class) {
-                    subclass(FlippedParamsStudent::class)
-                    subclass(Employee::class)
-                }
-            }
-        }
-
-        val db = PolymorphicDB(arrayOf(
-            Student("s00", "First name", "Last name"),
-            Employee(Student("s01", "First Name", "Last name"), 0)
-        ))
-
-        val file = tempFile()
-        serializationIon.encode(db, file.outputStream())
-
-        val integrityFailed = try {
-            deserializationIon.decode<PolymorphicDB>(file.inputStream())
-            false
-        } catch (e: IntegrityCheckException) {
-            true
-        }
-
-        assertTrue(integrityFailed)
-    }
 }
 
 @Serializable
@@ -82,10 +41,6 @@ interface Person {
 @Serializable
 @SerialName("student")
 data class Student(override val id: String, override val firstName: String, override val lastName: String): Person
-
-@Serializable
-@SerialName("student")
-data class FlippedParamsStudent(override val id: String, override val lastName: String, override val firstName: String): Person
 
 @Serializable
 data class Employee(val student: Person, private val index: Int): Person {
